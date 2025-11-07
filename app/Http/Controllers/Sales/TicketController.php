6bb9e618b9ee9e25044bb037382c8abea5d578b2
JaretEduardo <kaiilawlietmx@gmail.com>
2025-11-07 10:47:38 -0600

    build/refactorMigrations

--- app/Http/Controllers/Sales/TicketController.php
+++ app/Http/Controllers/Sales/TicketController.php
@@ -5,21 +5,32 @@ namespace App\Http\Controllers\Sales;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Sales\Ticket;
+use App\Models\User\User;
+use App\Models\User\Person;
+use Illuminate\Support\Facades\Validator;
 
 class TicketController extends Controller
 {
-    public function store(Request $request)
+    public function purchaseInShop(Request $request)
     {
-        $request->validate([
+        $purchaseInShop = Validator::make($request->all(), [
             'ticketID' => 'required|string|unique:tickets,ticketID',
             'items' => 'required|array',
             'items.*.name' => 'required|string',
             'items.*.price' => 'required|numeric',
             'items.*.quantity' => 'required|integer',
             'totalAmount' => 'required|numeric',
-            'cashier' => 'nullable|array',
+            'cashier' => 'required|array',
             'customer' => 'nullable|array',
         ]);
+
+        if ($purchaseInShop->fails()) {
+            return response()->json([
+                'result' => false,
+                'msg' => 'Error de validación.',
+                'data' => $purchaseInShop->errors()
+            ], 422);
+        }
 
         $ticketData = [
             'ticketID' => $request->input('ticketID'),
@@ -27,11 +38,16 @@ class TicketController extends Controller
             'totalAmount' => $request->input('totalAmount'),
             'cashier' => $request->input('cashier'), // Solo sera null si fue para recoger en tienda, sera hasta que coloque el producto en el gabinete
             'customer' => $request->input('customer'), // Será null si no viene, se podra llenar si es producto del pedido en linea para recoger en tienda
+            'status' => 'Complete',
             'date' => now()
         ];
 
         $ticket = Ticket::create($ticketData);
 
-        return response()->json($ticket, 201);
+        return response()->json([
+            'result' => true,
+            'msg' => 'Ticket registrado con éxito',
+            'data' => $ticket
+        ], 201);
     }
 }
--- database/migrations/local_pi/2014_10_12_000000_create_users_table.php
+++ database/migrations/local_pi/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('branch_id')->nullable()->constrained('branches')->onDelete('cascade');
             $table->foreignId('person_id')->constrained('persons')->onDelete('cascade');
             $table->timestamps();
         });
