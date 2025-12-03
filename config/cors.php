028a1d0949f7115f901311318a72e3eab6c5a572
JaretEduardo <kaiilawlietmx@gmail.com>
2025-12-03 17:19:54 -0600

    fix/originscors

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,https://www.tojoshop.com,https://tojoshop.com,https://admin.tojoshop.com,https://api.tojoshop.com
-')),
+    'allowed_origins' => [
+    'https://tojoshop.com',
+    'https://www.tojoshop.com',
+    'https://admin.tojoshop.com',
+    'https://api.tojoshop.com',
+],
 
     'allowed_origins_patterns' => [],
 
